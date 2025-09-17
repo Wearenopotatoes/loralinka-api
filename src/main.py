d8521040aec30bcf86037e8a18090605fdf064ee
N0Namesir <rivasmejiaeduardo@gmail.com>
2025-09-16 22:57:23 -0600

    new origins added

--- src/main.py
+++ src/main.py
@@ -22,7 +22,7 @@ app = FastAPI(
 # Add CORS middleware for documentation UI
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://127.0.0.1:5000", "http://localhost:5000", "http://loralink.live"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
